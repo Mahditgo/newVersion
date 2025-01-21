28593a3d647722bcf0a91abd9f34665e221ba12c
Mahdi Mahdavi <mahdi90056@gmail.com>
2025-01-21 21:01:07 +0330

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors')
 
 const salesRouter = require('./routes/salesRouter');
 const warehouseRouter = require('./routes/warehouseRouter');
@@ -11,6 +12,10 @@ dotenv.config();
 
 
 const app = express();
+app.use(cors({
+  origin :"http://localhost:5173" ,
+  credentials : true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
